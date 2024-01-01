60e690c75be8fc02b9bc6df1df77971fa1d2e9e4
rehaan <rehaan.sahay20@gmail.com>
2024-01-01 19:15:22 +0530

    added manual entry

--- project.py
+++ project.py
@@ -3,16 +3,12 @@ import glob
 pdfs = [f for f in glob.glob("*.pdf") if f != 'abc.pdf']
 writer = PdfWriter()
 for pdf in pdfs:
+    search = input(f"which word to search in {pdf}(all lowercase pls): ")
     reader = PdfReader(pdf)
     cover = reader.pages[0]
     writer.add_page(cover)
     writer.add_outline_item(title=pdf, page_number=writer.pages[-1])
-    if pdf == "Nikhil Menon - Planning Democracy_ Modern India's Quest for Development-Cambridge University Press (2022).pdf":
-        for page in reader.pages:
-            if 'women' in page.extract_text().lower():
-                    p = writer.add_page(page=page)
-    else:
-        for page in reader.pages:
-            if 'rukmini' in page.extract_text().lower():
-                    p = writer.add_page(page=page)
+    for page in reader.pages:
+        if search in page.extract_text().lower():
+                p = writer.add_page(page=page)
 writer.write("abc.pdf")
